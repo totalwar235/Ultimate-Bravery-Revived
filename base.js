095e270560a9cca4374bad37c36ff20bdfc1ca83
Reed N <reed.norstrom@gmail.com>
2018-08-17 13:21:38 -0500

    Update base.js

--- base.js
+++ base.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   var currentTab = 0; // Current tab is set to be the first tab (0)
   showTab(currentTab); // Display the current tab
-)};
+});
 
 function showTab(n) {
   // This function will display the specified tab of the form ...
