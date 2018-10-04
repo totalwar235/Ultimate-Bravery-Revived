949301b97c86d451326658f16b02e409bf559c1d
Reed N <rbnorstrom@pluto.dsu.edu>
2018-10-03 23:20:26 -0500

    api key

--- resources/js/dataRetrieval.js
+++ resources/js/dataRetrieval.js
@@ -3,7 +3,7 @@ const reqURL = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations
 let myHeaders = new Headers();
 myHeaders.append('Origin', "http://127.0.0.1:3000/");
 myHeaders.append('Accept-Charset', "application/x-www-form-urlencoded; charset=UTF-8");
-myHeaders.append('X-Riot-Token', 'RGAPI-c08b619b-e607-43ec-9fa3-59a456f5eb6c');
+myHeaders.append('X-Riot-Token', 'RGAPI-8d9d1e07-2af7-4504-9ea8-6f74cee122a2');
 myHeaders.append('Accept-Language', "en-US,en;q=0.9");
 myHeaders.append('User-Agent', "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
 
