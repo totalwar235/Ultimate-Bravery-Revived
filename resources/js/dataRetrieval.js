db18a3a9aa660d1ef619a7663c796ea3f6a75aae
Reed N <rbnorstrom@pluto.dsu.edu>
2018-10-03 23:25:46 -0500

    origin

--- resources/js/dataRetrieval.js
+++ resources/js/dataRetrieval.js
@@ -1,7 +1,7 @@
 const reqURL = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations";
 
 let myHeaders = new Headers();
-myHeaders.append('Origin', "http://127.0.0.1:3000/");
+myHeaders.append('Origin', "https://totalwar235.github.io/Ultimate-Bravery-Revived/");
 myHeaders.append('Accept-Charset', "application/x-www-form-urlencoded; charset=UTF-8");
 myHeaders.append('X-Riot-Token', 'RGAPI-8d9d1e07-2af7-4504-9ea8-6f74cee122a2');
 myHeaders.append('Accept-Language', "en-US,en;q=0.9");
